1f6504a9958f2d2395c164c3d73b872e73e0b07b
unknown <jimar@bluehive.com.ph>
2022-09-11 23:09:07 +0800

    first upload

---
+++ controllers/redditController.js
@@ -0,0 +1,73 @@
+const db = require('../config/db');
+
+module.exports.getSubmission = async (body) => {
+
+    const fetch = require('node-fetch');
+    let subreddit = body.subreddit
+    let limit = body.limit ? body.limit : 10
+    let url = `https://www.reddit.com/r/${subreddit}.json?limit=${limit}`;
+    let settings = { method: "Get" };
+
+    return Promise.resolve(fetch(url, settings))
+        .then(res => res.json())
+        .then(async (json) => {
+            // return json.data.children[0]
+            let children = json.data.children
+            let return_data = []
+            for(const child of children)
+            {
+                let temp = []
+                
+                // return child.data.favorite;
+                const existPost = await db('submission')
+                                        .where('reddit_id', child.data.id)
+                                        .then(([reddit]) => {
+                                            temp = reddit
+                                        })
+                if(existPost)
+                {
+                    const updatePost = await db('submission')
+                                            .where('reddit_id', child.data.id)
+                                            .update({
+                                                subreddit : child.data.subreddit,
+                                                title : child.data.title,
+                                                link : child.data.url,
+                                                ups : child.data.ups,
+                                                downs : child.data.downs
+                                            }).then(([reddit]) => {
+                                                temp = reddit
+                                            })
+                }
+                else{
+                    temp = {
+                        subreddit : child.data.subreddit,
+                        reddit_id : child.data.id,
+                        title : child.data.title,
+                        link : child.data.url,
+                        ups : child.data.ups,
+                        downs : child.data.downs,
+                        favorite : false
+                    }
+                    const createPost = await db('submission')
+                                            .insert(temp)
+                                            
+                }
+
+                return_data.push(temp)
+            }
+           
+            return return_data;
+        })
+
+}
+
+module.exports.toggleFavorite = async(body) => {
+    
+    const updatePost = await db('submission')
+                    .where('reddit_id', body.id)
+                    .update({
+                        favorite : body.favorite
+                    })
+
+    return ({ status: 'success', message: 'User created successfully' });
+}
